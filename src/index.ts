aa2a8ae295d3ce81750bb3c04e56742e46076a1a
jeremy <j.arias@motorcredito.com.do>
2023-10-28 10:13:09 -0400

    changing Porty

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import routes from './routes/index';
 import './db/models/index';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middlewares
 app.use(cors());
